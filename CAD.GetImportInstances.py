325f99a5acacec90e0782febc5b65a5ac5c36d6d
diegojsanchez <76917258+diegojsanchez@users.noreply.github.com>
2021-01-10 13:55:09 +0100

    Update and rename CAD.GetAll.py to CAD.GetImportInstances.py

--- CAD.GetImportInstances.py
+++ CAD.GetImportInstances.py
@@ -1,5 +1,5 @@
 '''
-CAD.GetAll 21/12/2020
+CAD.GetImportInstances
 Goal: Get all the import instances (CAD) and their names.
 diegojsanchez@gmail.com #masalladedynamo
 '''
@@ -12,7 +12,7 @@ clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
 doc = DocumentManager.Instance.CurrentDBDocument
 #mainbody
-ins = FilteredElementCollector(doc).OfClass(ImportInstance).ToElements() # Para obtener las instancias de los elementos importados
-nombres = [x.Category.Name for x in ins] # Para acceder al nombre del cad
+impInstances = FilteredElementCollector(doc).OfClass(ImportInstance).ToElements() # Para obtener las instancias de los elementos importados
+nombres = [x.Category.Name for x in impInstances] # Para acceder al nombre del cad
 #output
-OUT = ins, nombres
+OUT = impInstances, nombres
