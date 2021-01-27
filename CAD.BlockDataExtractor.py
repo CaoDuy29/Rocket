15df2f1b7fac1e059f49b400adc2bc1c987a12ba
diegojsanchez <76917258+diegojsanchez@users.noreply.github.com>
2021-01-27 13:24:57 +0100

    Update CAD.BlockDataExtractor.py

--- CAD.BlockDataExtractor.py
+++ CAD.BlockDataExtractor.py
@@ -29,7 +29,7 @@ def desplazamientoOrigen(geometrias):
 		return geometrias
 #inputs
 cad = UnwrapElement(IN[0]) #Necesita un ImportInstance
-geometrias, nombres, idCapas = [], [], []
+geometrias, nombreBloques, nombreCapas = [], [], []
 #mainbody
 	#acceder a los bloques
 opciones = cad.get_Geometry(Options()) #Accedemos a las opciones de geometría
@@ -41,13 +41,17 @@ elementos = [b.Symbol for b in bloques] #Obtenemos los elementType y accedemos a
 for e in elementos:
 	nombreCompleto = Element.Name.__get__(e) #El nombre completo tiene una estructura: nombre del archivo.dwg.nombre del bloque
 	lista = nombreCompleto.split(".") # Trocear string usando el punto.
-	nombres.append(lista[-1]) # El nombre del bloque esta al final de la lista
+	nombreBloques.append(lista[-1]) # El nombre del bloque esta al final de la lista
 listaGeometrias = [b.GetInstanceGeometry() for b in bloques] #Obtenemos todas las geometrías almacenadas por listas
 for lista in listaGeometrias: #Estas geometrias estan ubicadas respecto al ptoOrigenCAD tendre que desplazarlos al ptoOrigenRVT
+	aux1, aux2 = [], []
 	for g in lista:
-		idCapas.append(g.GraphicsStyleId) #Busco el id del estilo grafico o capa de las curvas
-		geometrias.append(g.ToProtoType()) #Almaceno las curvas
-capas = [doc.GetElement(id) for id in idCapas] #Conocido el id busco la capa
-nombreCapas = [s.GraphicsStyleCategory.Name for s in capas] #Busco el nombre de la capa de cada geometria
+		aux1.append(g.ToProtoType()) #Almaceno las curvas	
+		id = (g.GraphicsStyleId) #Busco el id del estilo grafico o capa de las curvas
+		capa = doc.GetElement(id) #Conocido el id busco la capa
+		nCapas = capa.GraphicsStyleCategory.Name #Busco el nombre de la capa de cada geometria
+		aux2.append(nCapas)
+	geometrias.append(aux1)
+	nombreCapas.append(aux2)
 #output
-OUT = desplazamientoOrigen(origen), nombres, desplazamientoOrigen(geometrias), nombreCapas
+OUT = desplazamientoOrigen(origen), nombreBloques, desplazamientoOrigen(geometrias), nombreCapas
