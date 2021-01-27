acc4c5f48633b51b90823663693f36436a0e37cf
diegojsanchez <76917258+diegojsanchez@users.noreply.github.com>
2021-01-27 10:06:25 +0100

    Update CAD.GetLayerNames.py

--- CAD.GetLayerNames.py
+++ CAD.GetLayerNames.py
@@ -4,7 +4,7 @@ Goal: Get all layer names of an import instance.
 diegojsanchez@gmail.com #masalladedynamo
 v: 1.0.1
 '''
-# bibliotecas
+#bibliotecas
 import clr 
 clr.AddReference('RevitAPI')
 import Autodesk
